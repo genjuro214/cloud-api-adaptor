59b864f638f6799f071f798c4df2cd5575b3d379
Lei Li <genjuro214@gmail.com>
2024-04-08 00:33:45 -0700

    build podvm image

--- src/cloud-api-adaptor/hack/build-s390x-image.sh
+++ src/cloud-api-adaptor/hack/build-s390x-image.sh
@@ -78,6 +78,7 @@ qemu-nbd --disconnect "${tmp_nbd}"
 
 output_img_name="podvm-s390x.qcow2"
 qemu-img convert -O qcow2 -c "${tmp_img_path}" "${output_img_name}"
+chmod 755 "${output_img_name}"
 
 output_img_path=$(realpath "${output_img_name}")
 echo "podvm image is generated: ${output_img_path}"
